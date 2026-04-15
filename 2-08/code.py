0abf78b40493b25aa141ea057bc91c042a8ea9ec
elliottroach <elliott.roach@stu.ocsb.ca>
2026-04-15 14:24:29 -0400

    fixed code to work

--- 2-08/code.py
+++ 2-08/code.py
@@ -1,7 +1,7 @@
 """
 Created by Elliott Roach
-Created on Mar 2026
-This is a sonar distance sensors and moves a servo if under 50 cm.
+Created on Apr 2026
+This is a potencheomitor that controls a servo servo
 """
 
 import board
@@ -12,19 +12,18 @@ from adafruit_motor import servo
 import analogio
 
 # setup
+potentiometer = analogio.AnalogIn(board.GP26)
 data_wire = pwmio.PWMOut(board.GP11, duty_cycle= 2 ** 15, frequency= 50)
 my_servo = servo.Servo(data_wire)
 my_servo.angle = 0
 servo_angle = 0
 ratio = 65535 / 180
-potentiometer = analogio.AnalogIn(board.GP26_A0)
+# 65535 is max resestance, 180 is max angle servo can go
 
-
-# infinte loop
 while True:
     try:
-            servo_angle = potentiometer / ratio
-            my_servo.angle = servo_angle
+        angle = (potentiometer.value) / ratio
+        my_servo.angle = angle
     except RuntimeError:
         print("Error")
     time.sleep(0.1)
